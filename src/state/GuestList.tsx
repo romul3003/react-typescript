2dfc94d30070baee08e386b0dadcfbf8f91b13df
Roman Zakrevskyi <r.zakrevskyi@weplay.tv>
2021-05-18 11:31:37 +0300

    State

---
+++ src/state/GuestList.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const GuestList: React.FC = () => {
+  const [name, setName] = useState('');
+  const [guests, setGuests] = useState<string[]>([]); // empty array shouldn't have type any
+
+  const onClick = () => {
+    setName('');
+    setGuests([...guests, name]);
+  };
+
+  return (
+    <div>
+      <h3>Guest List</h3>
+      <ul>
+        {guests.map((guest, i) => (
+          <li key={i}>{guest}</li>
+        ))}
+      </ul>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Add Guest</button>
+    </div>
+  );
+};
+
+export default GuestList;
